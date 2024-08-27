8b0bc279af791913c93b28c8a0d736f2bc50f950
Azamat0706 <azamatmirsobitov12@gmail.com>
2024-08-27 16:16:43 +0300

    finish portfolio

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+        <a href="https://t.me/AZIK_0706">Azamat Mirsobitov</a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
